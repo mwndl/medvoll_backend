00088ac49c92ad94f8bf6374894333bf726687d5
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2024-03-04 16:21:20 -0300

    conexão com o BD
    está conectado para MySQL, alterar para o BD do Mac

---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,12 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public class Medico {
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    private Especialidade especialidade;
+    private Endereco endereco;
+}
