11d97d814494193ae0c2b7efa1a051494950b50b
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2024-03-11 16:19:52 -0300

    Add files via upload

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -7,4 +7,24 @@ public class Endereco {
     private String complemento;
     private String cidade;
     private String uf;
+
+    public Endereco(){
+
+    }
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+
+        this.bairro = dados.bairro();
+
+        this.cep = dados.cep();
+
+        this.uf = dados.uf();
+
+        this.cidade = dados.cidade();
+
+        this.numero = dados.numero();
+
+        this.complemento = dados.complemento();
+    }
 }
